a0226e0069d33f959f6bd8150b124890dcb4d198
UsernameDavid <cuentadavid@live.com>
2023-11-08 18:38:58 +0100

    replaced the backend url

--- src/components/checkout/payment-form.js
+++ src/components/checkout/payment-form.js
@@ -29,7 +29,7 @@ const elements = useElements();
       console.log("paymentMethod", paymentMethod);
       const { id } = paymentMethod;
       try{
-        const { data } = await axios.post('https://verdant-sunburst-943bd5.netlify.app/api/checkout', {
+        const { data } = await axios.post('https://654bc61e5c613a1b12299848--celebrated-starship-bdf2e7.netlify.app/api/checkout', {
           id,
           amount: ((props.myCart.reduce((amount, item) => parseFloat(item.price) + parseFloat(amount), 0)).toFixed(2) * 100)
         }, { withCredentials: true });
